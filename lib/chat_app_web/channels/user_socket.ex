b12c377342b82ef97cdcec84af46800bcd52cf3b
chanraork_ig <chanra.ork@idealinkconsulting.com>
2021-06-05 14:58:24 +0700

    added log user_id when connect socket

--- lib/chat_app_web/channels/user_socket.ex
+++ lib/chat_app_web/channels/user_socket.ex
@@ -19,6 +19,8 @@ defmodule ChatAppWeb.UserSocket do
   def connect(%{"token" => token}, socket, _connection_info) do
     case Phoenix.Token.verify(socket, "user token", token, max_age: @max_age) do
       {:ok, user_id} ->
+        IO.puts("=========== connect =============")
+        IO.inspect(user_id)
         {:ok, assign(socket, :current_user_id, user_id)}
 
       {:error, _} ->
